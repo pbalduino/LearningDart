8030ecd88ca47f57d8853f704b7ddd1bbf118560
Plinio Balduino <pbalduino+github@gmail.com>
2012-05-28 00:13:33 -0300

    Playing with classes

--- LearningDart.dart
+++ LearningDart.dart
@@ -1,3 +1,16 @@
 void main() {
   print("Hello, World!");
+  var bleh = new Bleh();
+  bleh.meh();
+  bleh.foo("bar");
+}
+
+class Bleh {
+  void meh() {
+    print("meh");
+  }
+
+  void foo(String bar) {
+    print(bar);
+  }
 }
